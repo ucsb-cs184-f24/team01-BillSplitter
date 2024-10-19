09877705272e90f33da9d3997c03bdfbea32bd86
Liv Jonokuchi <107738534+livjono@users.noreply.github.com>
2024-10-18 18:23:49 -0700

    Create SecondScreen.js

---
+++ SecondScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SecondScreen = () => {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>This is the second page with different content.</Text>
+    </View>
+  );
+};
+
+export default SecondScreen;
