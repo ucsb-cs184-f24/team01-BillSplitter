2bb7c30ce0ed900aa937cb4f38550392b3ef8f9c
Liv Jonokuchi <107738534+livjono@users.noreply.github.com>
2024-10-18 18:14:54 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignInScreen from './SignInScreen';
+import HomeScreen from './HomeScreen';
+import SecondScreen from './SecondScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Sign In">
+        <Stack.Screen name="Sign In" component={SignInScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Second Page" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
