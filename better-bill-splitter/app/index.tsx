7ea5949a8e9f91e95c12ee2ee7bf002708476ab3
alexanderthomasucsb <alexanderthomas@ucsb.edu>
2024-10-21 15:01:09 -0700

    homepage bug fix

---
+++ better-bill-splitter/app/index.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View } from "react-native";
+import HomePage from "./homepage";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <HomePage />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
